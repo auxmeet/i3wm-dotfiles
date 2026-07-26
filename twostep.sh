4a191d84d7089c1f46d09ada736d8d0b3613080a
auxmeet <mit35826@gmail.com>
2026-07-26 10:40:05 +0900

    Update twostep.sh

--- twostep.sh
+++ twostep.sh
@@ -3,26 +3,26 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m'
-echo -e "${YELLOW}Установка picom и обоев...${NC}"
+echo -e "${YELLOW}Install picom-ftlabs-git and set wallpaper...${NC}"
 PACKAGESPARU=(
     "picom-ftlabs-git"
 )
-echo -e "${YELLOW}Обновление paru...${NC}"
+echo -e "${YELLOW}Update paru...${NC}"
 paru -Syu --noconfirm
-echo -e "${YELLOW}Установка утилиты picom-ftlabs-git и обоев${NC}"
+echo -e "${YELLOW}Install picom-ftlabs-git and set wallpaper...${NC}"
 for package in "${PACKAGESPARU[@]}"; do
     echo "Установка $package..."
     paru -S "$package" --noconfirm
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}✓ $package установлен${NC}"
+        echo -e "${GREEN}✓ $package installed${NC}"
     else
-        echo -e "${RED}✗ Ошибка при установке $package${NC}"
+        echo -e "${RED}✗ Error while installing $package${NC}"
     fi
 done
-echo -e "${YELLOW}Копирование обоев..${NC}"
+echo -e "${YELLOW}Copy wallpapers..${NC}"
 mkdir -p "$HOME/wallpapers/"
 cp wall.jpg "$HOME/wallpapers/"
-echo -e "${YELLOW}Копирование конфигов...${NC}"
+echo -e "${YELLOW}Copy dotfiles...${NC}"
 mkdir -p "$HOME/.config/"
 sudo cp -r -v rofi dunst kitty picom polybar i3 "$HOME/.config/"
-echo -e "${GREEN}✓ Все готово!${NC}"
+echo -e "${GREEN}✓ All set !${NC}"
