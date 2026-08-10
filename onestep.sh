405028d20af48ece1f51fe4fee98add596cc8992
auxmeet <mit35826@gmail.com>
2026-08-10 23:57:33 +0900

    Update onestep.sh

--- onestep.sh
+++ onestep.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-RED='\033[0;31m'
-NC='\033[0m'
-echo -e "${YELLOW}Installing utilities and dotfiles...${NC}"
+
+# Pacman Packages
 PACKAGESPACMAN=(
     "i3-wm"
     "rofi"
@@ -14,6 +11,7 @@ PACKAGESPACMAN=(
     "kitty"
     "maim"
     "feh"
+    "xclip"
     "fastfetch"
     "xorg-xsetroot"
     "xorg-xrandr"
@@ -22,15 +20,19 @@ PACKAGESPACMAN=(
     "base-devel"
     "ly"
 )
-echo -e "${YELLOW}Update pacman...${NC}"
-pacman -Syu --noconfirm --needed
-echo -e "${YELLOW}Installing utilities...${NC}"
+
+# Pacman Update
+echo -e "Update pacman..."
+pacman -Syu --noconfirm --needed > /dev/null 2>&1
+
+# Pacman Install
+echo -e "Installing utilities..."
 for package in "${PACKAGESPACMAN[@]}"; do
     echo "Installing $package..."
-    sudo pacman -S "$package" --noconfirm --needed
+    sudo pacman -S "$package" --noconfirm --needed > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}✓ $package installed${NC}"
+        echo -e "✓ $package installed"
     else
-        echo -e "${RED}✗ Error while installing $package${NC}"
+        echo -e "✗ Error while installing $package"
     fi
 done
